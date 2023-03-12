be7caf48cee14304b96bb3e9f1fefd5581bd068d
DanKhuda <dan.network.17@gmail.com>
2023-03-12 23:14:36 +0200

    changed env-related variables

--- src/pages/api/config.ts
+++ src/pages/api/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://www.omdbapi.com/";
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || "27969f5d";
